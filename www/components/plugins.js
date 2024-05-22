172a0cda25ae64c47f626d034a34bd82d8942e43
OthavioLuis <othaviorodriguesf@gmail.com>
2024-05-23 06:26:52 +0900

    Finalizado style do App de notificação

--- www/components/plugins.js
+++ www/components/plugins.js
@@ -1,25 +1,34 @@
 // This is a JavaScript file
-
 $(document).on("click", "#alerta", function(){
-    
     function retorno(){
         retorno
     }
-    navigator.notification.alert("Olá, jogador:", retorno, "Aviso!", "Aceito");
+    navigator.notification.alert("Que comece os jogos, só existe uma opção:", retorno, "Jogos Mortais", "Aceito");
 });
 $(document).on("click","#confirme", function(){
     function confirma(buttonIndex){
         if(buttonIndex == "1"){
+            navigator.notification.alert("Errou, jacaré foi pro lago e morreu ");
+        } else if(buttonIndex == "2") {
             navigator.notification.alert("Errou, jacaré atravessou a pista e morreu");
         } else {
-            navigator.notification.alert("Errou, jacaré foi pro lago e morreu");
+            navigator.notification.alert("Se saiu é pq tu é fraco!");
         }
     }
-    navigator.notification.confirm("Escolha jacaré vai para a PISTA(A) ou vai para o LAGO(B)", confirma, "Escolha:", ['A', 'B']);
+    navigator.notification.confirm("Escolha jacaré vai para a PISTA(A) ou vai para o LAGO(B)", confirma, "Escolha:", ['B', 'A']);
 });
 $(document).on("click","#beep",function(){
-    navigator.notification.beep(4);
+    if(navigator.notification.beep) {
+        navigator.notification.beep(4);
+    } else {
+        navigator.notification.alert("Seu dispositivo não é compatível, atualize o android ou jogue fora seu Iphone!");
+    }
 });
 $(document).on("click","#vibrar",function(){
-    navigator.vibrate.vibrar;
+    if(navigator.vibrate){
+        navigatior.vibrate(2000);
+    } else {
+        navigator.notification.alert("Seu dispositivo não é compatível, atualize o android ou jogue fora seu Iphone!");
+    }
+    
 });
